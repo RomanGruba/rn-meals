bdcaed1f301d6b3d11ecbd7729599af19a58dc5a
Roman Gruba <roman.gruba@gmail.com>
2020-02-10 00:44:02 +0200

    filters

--- components/DefaulText.js
+++ components/DefaulText.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 
 const DefaultText = props => {
-  return <Text style={styles.text}>{props.cheldren}</Text>;
+  return <Text style={styles.text}>{props.children}</Text>;
 };
 
 const styles = StyleSheet.create({
